2ad73df1da8452c07afd2bd4f65c6007509d0d13
Irasil <91846030+Irasil@users.noreply.github.com>
2022-11-05 22:14:05 +0100

    "Protection"

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -7,67 +9,90 @@ using Ski_Service_Management.Models;
 using Ski_Service_Management.Services;
 using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-var loggerFromSettings = new LoggerConfiguration()
-               .ReadFrom.Configuration(builder.Configuration)
-               .Enrich.FromLogContext()
-               .CreateLogger();
-
-builder.Logging.ClearProviders();
-builder.Logging.AddSerilog(loggerFromSettings);
-
-
-builder.Services.AddDbContext<ManagementContext>(options => 
-            options.UseSqlServer(builder.Configuration.GetConnectionString("MovieDB")));
-
-// Add services to the container.
-builder.Services.AddScoped<IRegistrationsService, RegistrationService>();
-builder.Services.AddScoped<IPriorityService, PriorityService>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IMitarbeiterService, MitarbeiterService>();
-
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+internal class Program
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWT", Version = "v1" });
-});
-
-//
-// JWT
-//
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+   
+    public static void Main(string[] args)
     {
-        options.TokenValidationParameters = new TokenValidationParameters
+
+       
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        var loggerFromSettings = new LoggerConfiguration()
+                       .ReadFrom.Configuration(builder.Configuration)
+                       .Enrich.FromLogContext()
+                       .CreateLogger();
+
+        builder.Logging.ClearProviders();
+        builder.Logging.AddSerilog(loggerFromSettings);
+       
+        
+        builder.Services.AddDbContext<ManagementContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieDB")));
+        var lol = builder.Configuration.GetConnectionString("MovieDB");
+        var streamReader = new StreamReader("TextFile.txt");
+        var line = streamReader.ReadLine();
+
+        var streamReader1 = new StreamReader("TextFile1.txt");
+        var line1 = streamReader1.ReadLine();
+
+        var provider = DataProtectionProvider.Create(line);
+        var protector = provider.CreateProtector(line);
+        
+        string deencrypted = protector.Unprotect(line1);
+        var hey = string.Format(lol, deencrypted);
+         
+        // Add services to the container.
+        builder.Services.AddScoped<IRegistrationsService, RegistrationService>();
+        builder.Services.AddScoped<IPriorityService, PriorityService>();
+        builder.Services.AddScoped<ITokenService, TokenService>();
+        builder.Services.AddScoped<IMitarbeiterService, MitarbeiterService>();
+
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
         {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWT", Version = "v1" });
+        });
+
+        builder.Services.AddDataProtection();
+
+        //
+        // JWT
+        //
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
 
-var app = builder.Build();
+        var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
